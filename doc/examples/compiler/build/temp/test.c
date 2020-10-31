8382776e829e4361d1de03897c8ef2e25397bad3
Kha <Kha@users.noreply.github.com>
2020-10-31 12:51:13 +0000

    deploy: 9d78e2268db285bd88280558d4079ed982bb99b4

---
+++ doc/examples/compiler/build/temp/test.c
@@ -0,0 +1,323 @@
+// Lean compiler output
+// Module: test
+// Imports: Init
+#include <lean/lean.h>
+#if defined(__clang__)
+#pragma clang diagnostic ignored "-Wunused-parameter"
+#pragma clang diagnostic ignored "-Wunused-label"
+#elif defined(__GNUC__) && !defined(__CLANG__)
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+#pragma GCC diagnostic ignored "-Wunused-label"
+#pragma GCC diagnostic ignored "-Wunused-but-set-variable"
+#endif
+#ifdef __cplusplus
+extern "C" {
+#endif
+lean_object* lean_string_push(lean_object*, uint32_t);
+extern lean_object* l_List_repr___rarg___closed__1;
+lean_object* _lean_main(lean_object*, lean_object*);
+lean_object* l_IO_println___at_main___spec__3(lean_object*, lean_object*);
+lean_object* lean_string_append(lean_object*, lean_object*);
+extern lean_object* l_List_repr___rarg___closed__3;
+lean_object* l_IO_getStdout___at_main___spec__5(lean_object*);
+lean_object* l_main___boxed__const__1;
+lean_object* l_IO_print___at_main___spec__4(lean_object*, lean_object*);
+extern lean_object* l_List_repr___rarg___closed__2;
+lean_object* l_List_toString___at_main___spec__1(lean_object*);
+extern lean_object* l_String_splitAux___closed__1;
+lean_object* lean_get_stdout(lean_object*);
+extern lean_object* l_List_reprAux___rarg___closed__1;
+lean_object* l_List_toStringAux___at_main___spec__2___boxed(lean_object*, lean_object*);
+lean_object* l_List_toStringAux___at_main___spec__2(uint8_t, lean_object*);
+lean_object* l_List_toStringAux___at_main___spec__2(uint8_t x_1, lean_object* x_2) {
+_start:
+{
+if (x_1 == 0)
+{
+if (lean_obj_tag(x_2) == 0)
+{
+lean_object* x_3; 
+x_3 = l_String_splitAux___closed__1;
+return x_3;
+}
+else
+{
+lean_object* x_4; lean_object* x_5; lean_object* x_6; lean_object* x_7; uint8_t x_8; lean_object* x_9; lean_object* x_10; 
+x_4 = lean_ctor_get(x_2, 0);
+lean_inc(x_4);
+x_5 = lean_ctor_get(x_2, 1);
+lean_inc(x_5);
+lean_dec(x_2);
+x_6 = l_List_reprAux___rarg___closed__1;
+x_7 = lean_string_append(x_6, x_4);
+lean_dec(x_4);
+x_8 = 0;
+x_9 = l_List_toStringAux___at_main___spec__2(x_8, x_5);
+x_10 = lean_string_append(x_7, x_9);
+lean_dec(x_9);
+return x_10;
+}
+}
+else
+{
+if (lean_obj_tag(x_2) == 0)
+{
+lean_object* x_11; 
+x_11 = l_String_splitAux___closed__1;
+return x_11;
+}
+else
+{
+lean_object* x_12; lean_object* x_13; uint8_t x_14; lean_object* x_15; lean_object* x_16; 
+x_12 = lean_ctor_get(x_2, 0);
+lean_inc(x_12);
+x_13 = lean_ctor_get(x_2, 1);
+lean_inc(x_13);
+lean_dec(x_2);
+x_14 = 0;
+x_15 = l_List_toStringAux___at_main___spec__2(x_14, x_13);
+x_16 = lean_string_append(x_12, x_15);
+lean_dec(x_15);
+return x_16;
+}
+}
+}
+}
+lean_object* l_List_toString___at_main___spec__1(lean_object* x_1) {
+_start:
+{
+if (lean_obj_tag(x_1) == 0)
+{
+lean_object* x_2; 
+x_2 = l_List_repr___rarg___closed__1;
+return x_2;
+}
+else
+{
+uint8_t x_3; 
+x_3 = !lean_is_exclusive(x_1);
+if (x_3 == 0)
+{
+uint8_t x_4; lean_object* x_5; lean_object* x_6; lean_object* x_7; lean_object* x_8; lean_object* x_9; 
+x_4 = 1;
+x_5 = l_List_toStringAux___at_main___spec__2(x_4, x_1);
+x_6 = l_List_repr___rarg___closed__2;
+x_7 = lean_string_append(x_6, x_5);
+lean_dec(x_5);
+x_8 = l_List_repr___rarg___closed__3;
+x_9 = lean_string_append(x_7, x_8);
+return x_9;
+}
+else
+{
+lean_object* x_10; lean_object* x_11; lean_object* x_12; uint8_t x_13; lean_object* x_14; lean_object* x_15; lean_object* x_16; lean_object* x_17; lean_object* x_18; 
+x_10 = lean_ctor_get(x_1, 0);
+x_11 = lean_ctor_get(x_1, 1);
+lean_inc(x_11);
+lean_inc(x_10);
+lean_dec(x_1);
+x_12 = lean_alloc_ctor(1, 2, 0);
+lean_ctor_set(x_12, 0, x_10);
+lean_ctor_set(x_12, 1, x_11);
+x_13 = 1;
+x_14 = l_List_toStringAux___at_main___spec__2(x_13, x_12);
+x_15 = l_List_repr___rarg___closed__2;
+x_16 = lean_string_append(x_15, x_14);
+lean_dec(x_14);
+x_17 = l_List_repr___rarg___closed__3;
+x_18 = lean_string_append(x_16, x_17);
+return x_18;
+}
+}
+}
+}
+lean_object* l_IO_getStdout___at_main___spec__5(lean_object* x_1) {
+_start:
+{
+lean_object* x_2; 
+x_2 = lean_get_stdout(x_1);
+return x_2;
+}
+}
+lean_object* l_IO_print___at_main___spec__4(lean_object* x_1, lean_object* x_2) {
+_start:
+{
+lean_object* x_3; 
+x_3 = lean_get_stdout(x_2);
+if (lean_obj_tag(x_3) == 0)
+{
+lean_object* x_4; lean_object* x_5; lean_object* x_6; lean_object* x_7; 
+x_4 = lean_ctor_get(x_3, 0);
+lean_inc(x_4);
+x_5 = lean_ctor_get(x_3, 1);
+lean_inc(x_5);
+lean_dec(x_3);
+x_6 = lean_ctor_get(x_4, 5);
+lean_inc(x_6);
+lean_dec(x_4);
+x_7 = lean_apply_2(x_6, x_1, x_5);
+return x_7;
+}
+else
+{
+uint8_t x_8; 
+lean_dec(x_1);
+x_8 = !lean_is_exclusive(x_3);
+if (x_8 == 0)
+{
+return x_3;
+}
+else
+{
+lean_object* x_9; lean_object* x_10; lean_object* x_11; 
+x_9 = lean_ctor_get(x_3, 0);
+x_10 = lean_ctor_get(x_3, 1);
+lean_inc(x_10);
+lean_inc(x_9);
+lean_dec(x_3);
+x_11 = lean_alloc_ctor(1, 2, 0);
+lean_ctor_set(x_11, 0, x_9);
+lean_ctor_set(x_11, 1, x_10);
+return x_11;
+}
+}
+}
+}
+lean_object* l_IO_println___at_main___spec__3(lean_object* x_1, lean_object* x_2) {
+_start:
+{
+uint32_t x_3; lean_object* x_4; lean_object* x_5; 
+x_3 = 10;
+x_4 = lean_string_push(x_1, x_3);
+x_5 = l_IO_print___at_main___spec__4(x_4, x_2);
+return x_5;
+}
+}
+static lean_object* _init_l_main___boxed__const__1() {
+_start:
+{
+uint32_t x_1; lean_object* x_2; 
+x_1 = 0;
+x_2 = lean_box_uint32(x_1);
+return x_2;
+}
+}
+lean_object* _lean_main(lean_object* x_1, lean_object* x_2) {
+_start:
+{
+lean_object* x_3; lean_object* x_4; 
+x_3 = l_List_toString___at_main___spec__1(x_1);
+x_4 = l_IO_println___at_main___spec__3(x_3, x_2);
+if (lean_obj_tag(x_4) == 0)
+{
+uint8_t x_5; 
+x_5 = !lean_is_exclusive(x_4);
+if (x_5 == 0)
+{
+lean_object* x_6; lean_object* x_7; 
+x_6 = lean_ctor_get(x_4, 0);
+lean_dec(x_6);
+x_7 = l_main___boxed__const__1;
+lean_ctor_set(x_4, 0, x_7);
+return x_4;
+}
+else
+{
+lean_object* x_8; lean_object* x_9; lean_object* x_10; 
+x_8 = lean_ctor_get(x_4, 1);
+lean_inc(x_8);
+lean_dec(x_4);
+x_9 = l_main___boxed__const__1;
+x_10 = lean_alloc_ctor(0, 2, 0);
+lean_ctor_set(x_10, 0, x_9);
+lean_ctor_set(x_10, 1, x_8);
+return x_10;
+}
+}
+else
+{
+uint8_t x_11; 
+x_11 = !lean_is_exclusive(x_4);
+if (x_11 == 0)
+{
+return x_4;
+}
+else
+{
+lean_object* x_12; lean_object* x_13; lean_object* x_14; 
+x_12 = lean_ctor_get(x_4, 0);
+x_13 = lean_ctor_get(x_4, 1);
+lean_inc(x_13);
+lean_inc(x_12);
+lean_dec(x_4);
+x_14 = lean_alloc_ctor(1, 2, 0);
+lean_ctor_set(x_14, 0, x_12);
+lean_ctor_set(x_14, 1, x_13);
+return x_14;
+}
+}
+}
+}
+lean_object* l_List_toStringAux___at_main___spec__2___boxed(lean_object* x_1, lean_object* x_2) {
+_start:
+{
+uint8_t x_3; lean_object* x_4; 
+x_3 = lean_unbox(x_1);
+lean_dec(x_1);
+x_4 = l_List_toStringAux___at_main___spec__2(x_3, x_2);
+return x_4;
+}
+}
+lean_object* initialize_Init(lean_object*);
+static bool _G_initialized = false;
+lean_object* initialize_test(lean_object* w) {
+lean_object * res;
+if (_G_initialized) return lean_io_result_mk_ok(lean_box(0));
+_G_initialized = true;
+res = initialize_Init(lean_io_mk_world());
+if (lean_io_result_is_error(res)) return res;
+lean_dec_ref(res);
+l_main___boxed__const__1 = _init_l_main___boxed__const__1();
+lean_mark_persistent(l_main___boxed__const__1);
+return lean_io_result_mk_ok(lean_box(0));
+}
+void lean_initialize_runtime_module();
+
+  #if defined(WIN32) || defined(_WIN32)
+  #include <windows.h>
+  #endif
+
+  int main(int argc, char ** argv) {
+  #if defined(WIN32) || defined(_WIN32)
+  SetErrorMode(SEM_FAILCRITICALERRORS);
+  #endif
+  lean_object* in; lean_object* res;
+lean_initialize_runtime_module();
+res = initialize_test(lean_io_mk_world());
+lean_io_mark_end_initialization();
+if (lean_io_result_is_ok(res)) {
+lean_dec_ref(res);
+lean_init_task_manager();
+in = lean_box(0);
+int i = argc;
+while (i > 1) {
+ lean_object* n;
+ i--;
+ n = lean_alloc_ctor(1,2,0); lean_ctor_set(n, 0, lean_mk_string(argv[i])); lean_ctor_set(n, 1, in);
+ in = n;
+}
+res = _lean_main(in, lean_io_mk_world());
+}
+if (lean_io_result_is_ok(res)) {
+  int ret = lean_unbox(lean_io_result_get_value(res));
+  lean_dec_ref(res);
+  return ret;
+} else {
+  lean_io_result_show_error(res);
+  lean_dec_ref(res);
+  return 1;
+}
+}
+#ifdef __cplusplus
+}
+#endif
